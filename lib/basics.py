002c47b7a1b758f55704c4c61aa54881348c8ce0
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2021-03-06 11:40:30 +0000

    first commit

---
+++ lib/basics.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+
+import board
+import neopixel
+import time
+
+SIZE = 50
+pixels = neopixel.NeoPixel(board.D18, SIZE)
+
+
+def reset():
+    for i in range(SIZE):
+        pixels[i] = (0, 0, 0)
+
+
+def ligth_monocrome(color=(255, 0, 0), delay=0.05, reset=True):
+    if reset:
+        reset()
+    for i in range(SIZE):
+        pixels[i] = color
+        time.sleep(delay)
+
+
+def right_left_right(color1=(255, 0, 0), color2=(0,0,255), delay=0.05):
+    reset()
+    for i in range(SIZE):
+        pixels[i] = color1
+        time.sleep(delay)
+    for i in reversed(range(SIZE)):
+        pixels[i] = color2
+        time.sleep(delay)
+
+
+def move_with_tail(delay=0.1, lenght=5, reverse=False):
+     if not reverse:
+         for i in range(SIZE):
+             if i - lenght >= 0:
+                 pixels[i-lenght] = (0, 0, 0)
+             pixels[i] = (255, 0, 0)
+             time.sleep(delay)
+     else:
+         for i in reversed(range(SIZE)):
+             if i + lenght < SIZE:
+                  pixels[i+lenght] = (0, 0, 0)
+             pixels[i] = (255, 0, 0)
+             time.sleep(delay)
+
+
+def go_and_back(delay=0.05, lenght=5, times=10):
+    reset()
+    for _ in range(times):
+        move_with_tail(delay, lenght)
+        move_with_tail(delay, lenght, True)
+              
