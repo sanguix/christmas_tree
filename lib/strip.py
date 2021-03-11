52e5b7e08c4812a0d3871144785860957a8f305d
Sergio Merino <sergio.merino.a@gmail.com>
2021-03-11 22:19:21 +0000

    Change the order of pixels to RGB

--- lib/strip.py
+++ lib/strip.py
@@ -5,10 +5,11 @@ import board
 import neopixel
 
 class Strip(object):
+    PIXELS_ORDER = neopixel.RGB
     def __init__(self, size=50, pin=board.D18):
         self.size = size
         self.pin = pin
-        self.pixels = neopixel.NeoPixel(pin, size)
+        self.pixels = neopixel.NeoPixel(pin=pin, n=size, pixel_order=self.PIXELS_ORDER)
 
     def reset_lights(self):
         self.pixels.fill((0, 0, 0))
@@ -34,7 +35,7 @@ class Strip(object):
             time.sleep(delay)
 
     def draw_fadeout_line(self, center, length, central_color=(255, 255, 255)):
-        strip = neopixel.NeoPixel(self.pin, self.size, auto_write=False)
+        strip = neopixel.NeoPixel(self.pin, self.size, pixel_order=self.PIXELS_ORDER, auto_write=False)
 
         radio = int(length/2) + 1
         color_step = [x/radio for x in central_color]
