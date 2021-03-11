c83677514e8f17a59e2e1899f59154daf3528b69
Sergio Merino <sergio.merino.a@gmail.com>
2021-03-11 21:54:08 +0000

    Rename strip variable by pixels

--- lib/strip.py
+++ lib/strip.py
@@ -8,29 +8,29 @@ class Strip(object):
     def __init__(self, size=50, pin=board.D18):
         self.size = size
         self.pin = pin
-        self.strip = neopixel.NeoPixel(pin, size)
+        self.pixels = neopixel.NeoPixel(pin, size)
 
     def reset_lights(self):
-        self.strip.fill((0, 0, 0))
+        self.pixels.fill((0, 0, 0))
 
     def monochrome(self, color=(255, 0, 0), delay=0.05, reset_before=True):
         if reset_before:
             self.reset_lights()
 
         if delay == 0:
-            self.strip.fill(color)
+            self.pixels.fill(color)
         else:
             for i in range(self.size):
-                self.strip[i] = color
+                self.pixels[i] = color
                 time.sleep(delay)
 
     def right_left_right(self, color1=(255, 0, 0), color2=(0, 0, 255), delay=0.05):
         self.reset_lights()
         for i in range(self.size):
-            self.strip[i] = color1
+            self.pixels[i] = color1
             time.sleep(delay)
         for i in reversed(range(self.size)):
-            self.strip[i] = color2
+            self.pixels[i] = color2
             time.sleep(delay)
 
     def draw_fadeout_line(self, center, length, central_color=(255, 255, 255)):
