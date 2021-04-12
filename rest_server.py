0b617c08d9a043f57c8337cbcea9e9ea332d1ffc
Sergio Merino <sergio.merino.a@gmail.com>
2021-04-12 21:48:17 +0100

    Add go_and_back end point

--- rest_server.py
+++ rest_server.py
@@ -41,6 +41,18 @@ def turn_off():
     return "OK"
 
 
+@app.route("/go_and_back", methods=['POST'])
+def go_and_back():
+    data = request.get_json(force=True)
+    color = hexa_to_rgb(data.get('color'))
+    strip.go_and_back(color=color, times=3)
+    return "OK"
+
+
+def hexa_to_rgb(hex_color):
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
 
