5064f660bd35988740ccaaf781e535dcbcedf9f9
Sergio Merino <sergio.merino.a@gmail.com>
2021-05-02 19:27:56 +0100

    Once hexadecimal is used, there is no need to check color

--- rest_server.py
+++ rest_server.py
@@ -8,29 +8,10 @@ app = Flask(__name__)
 
 strip = Strip()
 
-def check_color(color):
-    if not isinstance(color, list):
-        raise TypeError(f'Color should be a list type variable')
-
-    if len(color) != 3:
-        raise ValueError(f'Color should be a list of 3 integer, not {color}')
-
-    for item in color:
-        if not isinstance(item, int):
-            raise ValueError(f'Color should be a list of 3 integer, but {item} was found')
-
-        if item < 0 or item > 255:
-            raise ValueError(f'Color values should be between 0 and 255, but {item} was found')
-
 @app.route("/progress_fill", methods=['POST'])
 def progress_fill():
     data = request.get_json(force=True)
-    color = data.get('color')
-    try:
-        check_color(color)
-    except Exception as e:
-        return str(e), 400
-
+    color = hexa_to_rgb(data.get('color'))
     strip.progressive_fill(color=color)
     return "OK"
 
